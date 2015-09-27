7f29f0627f1a49e5e5bff87c8cecabe7a504774e
Justas T <j.trimailovas@gmail.com>
2015-09-27 22:38:36 +0300

    Refactor current code into function

--- main.py
+++ main.py
@@ -25,54 +25,55 @@ def find_closest(value):
     return 64 * floor(value/64)
 
 
-img = Image.open('lena.bmp')
-img_data = img.getdata()
+def color_dither():
+    img = Image.open('lena.bmp')
+    img_data = img.getdata()
 
-new_img = Image.new('RGB', img.size, 'black')
-new_pixel = new_img.load()
+    new_img = Image.new('RGB', img.size, 'black')
+    new_pixel = new_img.load()
 
-x_lim, y_lim = img.size
+    x_lim, y_lim = img.size
 
-pixel = img_data.getpixel
+    pixel = img_data.getpixel
 
-for y in range(1, y_lim):
-    for x in range(1, x_lim):
-        red_oldpixel, green_oldpixel, blue_oldpixel = pixel((x, y))
+    for y in range(1, y_lim):
+        for x in range(1, x_lim):
+            red_oldpixel, green_oldpixel, blue_oldpixel = pixel((x, y))
 
-        red_newpixel = find_closest(red_oldpixel)
-        green_newpixel = find_closest(green_oldpixel)
-        blue_newpixel = find_closest(blue_oldpixel)
+            red_newpixel = find_closest(red_oldpixel)
+            green_newpixel = find_closest(green_oldpixel)
+            blue_newpixel = find_closest(blue_oldpixel)
 
-        red_error = red_oldpixel - red_newpixel
-        blue_error = blue_oldpixel - blue_newpixel
-        green_error = green_oldpixel - green_newpixel
+            red_error = red_oldpixel - red_newpixel
+            blue_error = blue_oldpixel - blue_newpixel
+            green_error = green_oldpixel - green_newpixel
 
-        if x < x_lim - 1:
-            red = pixel((x+1, y))[0] + floor(red_error * 7/16)
-            green = pixel((x+1, y))[1] + floor(green_error * 7/16)
-            blue = pixel((x+1, y))[2] + floor(blue_error * 7/16)
+            if x < x_lim - 1:
+                red = pixel((x+1, y))[0] + floor(red_error * 7/16)
+                green = pixel((x+1, y))[1] + floor(green_error * 7/16)
+                blue = pixel((x+1, y))[2] + floor(blue_error * 7/16)
 
-            new_pixel[x, y-1] = (red, green, blue)
+                new_pixel[x, y-1] = (red, green, blue)
 
-        if x > 1 and y < y_lim - 1:
-            red = pixel((x-1, y+1))[0] + floor(red_error * 3/16)
-            green = pixel((x-1, y+1))[1] + floor(green_error * 3/16)
-            blue = pixel((x-1, y+1))[2] + floor(blue_error * 3/16)
+            if x > 1 and y < y_lim - 1:
+                red = pixel((x-1, y+1))[0] + floor(red_error * 3/16)
+                green = pixel((x-1, y+1))[1] + floor(green_error * 3/16)
+                blue = pixel((x-1, y+1))[2] + floor(blue_error * 3/16)
 
-            new_pixel[x-2, y] = (red, green, blue)
+                new_pixel[x-2, y] = (red, green, blue)
 
-        if y < y_lim - 1:
-            red = pixel((x, y+1))[0] + floor(red_error * 5/16)
-            green = pixel((x, y+1))[1] + floor(green_error * 5/16)
-            blue = pixel((x, y+1))[2] + floor(blue_error * 5/16)
+            if y < y_lim - 1:
+                red = pixel((x, y+1))[0] + floor(red_error * 5/16)
+                green = pixel((x, y+1))[1] + floor(green_error * 5/16)
+                blue = pixel((x, y+1))[2] + floor(blue_error * 5/16)
 
-            new_pixel[x-1, y] = (red, green, blue)
+                new_pixel[x-1, y] = (red, green, blue)
 
-        if x < x_lim - 1 and y < y_lim - 1:
-            red = pixel((x+1, y+1))[0] + floor(red_error * 1/16)
-            green = pixel((x+1, y+1))[1] + floor(green_error * 1/16)
-            blue = pixel((x+1, y+1))[2] + floor(blue_error * 1/16)
+            if x < x_lim - 1 and y < y_lim - 1:
+                red = pixel((x+1, y+1))[0] + floor(red_error * 1/16)
+                green = pixel((x+1, y+1))[1] + floor(green_error * 1/16)
+                blue = pixel((x+1, y+1))[2] + floor(blue_error * 1/16)
 
-            new_pixel[x, y] = (red, green, blue)
+                new_pixel[x, y] = (red, green, blue)
 
-new_img.show()
+    new_img.show()
