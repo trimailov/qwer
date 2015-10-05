17206378eb1037292f9fce14010427722f840eb8
Justas T <j.trimailovas@gmail.com>
2015-10-06 00:52:45 +0300

    Refactor ditherin code into class

--- main.py
+++ main.py
@@ -21,104 +21,94 @@ from argparse import ArgumentParser
 from math import floor
 from PIL import Image
 
-import sys
+import os
 
 
-def apply_threshold(value):
-    "Returns 0 or 255 depending where value is closer"
-    return 255 * floor(value/128)
+class Dither():
+    def __init__(self, path, algorithm, save):
+        self.path = get_path(path)
+        self.func = get_func(algorithm)
+        self.func(self.path, save=save)
 
+    def get_path(self, path):
+        """Get whole path of an image
 
-def color_dither(image_file):
-    new_img = Image.open(image_file)
+        If path does not start with '/', then try to open image from pwd
+        If path starts with '/', then open image of given path
+        """
+        if path.startswith('/') and not path.startswith('~/'):
+            return os.getcwd() + '/' + path
+        else:
+            return path
 
-    new_img = new_img.convert('RGB')
-    pixel = new_img.load()
+    def get_func(self, algorithm):
+        "Get dithering function to run"
+        return floyd_steingberg
 
-    x_lim, y_lim = new_img.size
+    def apply_threshold(self, value):
+        "Returns 0 or 255 depending where value is closer"
+        return 255 * floor(value/128)
 
-    for y in range(1, y_lim):
-        for x in range(1, x_lim):
-            red_oldpixel, green_oldpixel, blue_oldpixel = pixel[x, y]
+    def floyd_steinberg_dither(self, image_file, save=False):
+        new_img = Image.open(image_file)
 
-            red_newpixel = apply_threshold(red_oldpixel)
-            green_newpixel = apply_threshold(green_oldpixel)
-            blue_newpixel = apply_threshold(blue_oldpixel)
+        new_img = new_img.convert('RGB')
+        pixel = new_img.load()
 
-            pixel[x, y] = red_newpixel, green_newpixel, blue_newpixel
+        x_lim, y_lim = new_img.size
 
-            red_error = red_oldpixel - red_newpixel
-            blue_error = blue_oldpixel - blue_newpixel
-            green_error = green_oldpixel - green_newpixel
+        for y in range(1, y_lim):
+            for x in range(1, x_lim):
+                red_oldpixel, green_oldpixel, blue_oldpixel = pixel[x, y]
 
-            if x < x_lim - 1:
-                red = pixel[x+1, y][0] + round(red_error * 7/16)
-                green = pixel[x+1, y][1] + round(green_error * 7/16)
-                blue = pixel[x+1, y][2] + round(blue_error * 7/16)
+                red_newpixel = self.apply_threshold(red_oldpixel)
+                green_newpixel = self.apply_threshold(green_oldpixel)
+                blue_newpixel = self.apply_threshold(blue_oldpixel)
 
-                pixel[x+1, y] = (red, green, blue)
+                pixel[x, y] = red_newpixel, green_newpixel, blue_newpixel
 
-            if x > 1 and y < y_lim - 1:
-                red = pixel[x-1, y+1][0] + round(red_error * 3/16)
-                green = pixel[x-1, y+1][1] + round(green_error * 3/16)
-                blue = pixel[x-1, y+1][2] + round(blue_error * 3/16)
+                red_error = red_oldpixel - red_newpixel
+                blue_error = blue_oldpixel - blue_newpixel
+                green_error = green_oldpixel - green_newpixel
 
-                pixel[x-1, y+1] = (red, green, blue)
+                if x < x_lim - 1:
+                    red = pixel[x+1, y][0] + round(red_error * 7/16)
+                    green = pixel[x+1, y][1] + round(green_error * 7/16)
+                    blue = pixel[x+1, y][2] + round(blue_error * 7/16)
 
-            if y < y_lim - 1:
-                red = pixel[x, y+1][0] + round(red_error * 5/16)
-                green = pixel[x, y+1][1] + round(green_error * 5/16)
-                blue = pixel[x, y+1][2] + round(blue_error * 5/16)
+                    pixel[x+1, y] = (red, green, blue)
 
-                pixel[x, y+1] = (red, green, blue)
+                if x > 1 and y < y_lim - 1:
+                    red = pixel[x-1, y+1][0] + round(red_error * 3/16)
+                    green = pixel[x-1, y+1][1] + round(green_error * 3/16)
+                    blue = pixel[x-1, y+1][2] + round(blue_error * 3/16)
 
-            if x < x_lim - 1 and y < y_lim - 1:
-                red = pixel[x+1, y+1][0] + round(red_error * 1/16)
-                green = pixel[x+1, y+1][1] + round(green_error * 1/16)
-                blue = pixel[x+1, y+1][2] + round(blue_error * 1/16)
+                    pixel[x-1, y+1] = (red, green, blue)
 
-                pixel[x+1, y+1] = (red, green, blue)
+                if y < y_lim - 1:
+                    red = pixel[x, y+1][0] + round(red_error * 5/16)
+                    green = pixel[x, y+1][1] + round(green_error * 5/16)
+                    blue = pixel[x, y+1][2] + round(blue_error * 5/16)
 
-    new_img.show()
+                    pixel[x, y+1] = (red, green, blue)
 
+                if x < x_lim - 1 and y < y_lim - 1:
+                    red = pixel[x+1, y+1][0] + round(red_error * 1/16)
+                    green = pixel[x+1, y+1][1] + round(green_error * 1/16)
+                    blue = pixel[x+1, y+1][2] + round(blue_error * 1/16)
 
-def bw_dither(image_file):
-    img = Image.open(image_file)
+                    pixel[x+1, y+1] = (red, green, blue)
 
-    new_img = img.convert('L')
-    pixel = new_img.load()
-
-    x_lim, y_lim = img.size
-
-    for y in range(0, y_lim):
-        for x in range(0, x_lim):
-            oldpixel = pixel[x, y]
-            newpixel = apply_threshold(oldpixel)
-            pixel[x, y] = newpixel
-            error = oldpixel - newpixel
-
-            if x < x_lim - 1:
-                pixel[x+1, y] += round(error * 7/16)
-
-            if x > 1 and y < y_lim - 1:
-                pixel[x-1, y+1] += round(error * 3/16)
-
-            if y < y_lim - 1:
-                pixel[x, y+1] += round(error * 5/16)
-
-            if x < x_lim - 1 and y < y_lim - 1:
-                pixel[x+1, y+1] += round(error * 1/16)
-
-    new_img.show()
+        new_img.show()
 
 
 def main():
     parser = ArgumentParser(description="Image dithering in python")
-    parser.add_argument("image", help="input image location")
+    parser.add_argument("image_path", help="input image location")
     parser.add_argument("-o", help="output image location")
     args = parser.parse_args()
 
-    if args.image:
-        color_dither(args.image)
-    elif: args.image and args.o:
-        color_dither(args.image, save=True)
+    if args.image_path:
+        Dither(args.image_path, algorithm='', save=False)
+    elif args.image_path and args.o:
+        Dither(args.image, algorithm='', save=True)
