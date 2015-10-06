73755a1b456eab2889958ff861a1274dca358816
Justas T <j.trimailovas@gmail.com>
2015-10-06 09:27:17 +0300

    Move comment into dither method

--- main.py
+++ main.py
@@ -1,22 +1,5 @@
 #!/usr/bin/python3
 
-# https://en.wikipedia.org/wiki/Floyd–Steinberg_dithering
-#
-# Pseudocode:
-#
-# for each y from top to bottom
-#    for each x from left to right
-#       oldpixel  := pixel[x][y]
-#       newpixel  := find_closest_palette_color(oldpixel)
-#       pixel[x][y]  := newpixel
-#       quant_error  := oldpixel - newpixel
-#       pixel[x+1][y  ] := pixel[x+1][y  ] + quant_error * 7/16
-#       pixel[x-1][y+1] := pixel[x-1][y+1] + quant_error * 3/16
-#       pixel[x  ][y+1] := pixel[x  ][y+1] + quant_error * 5/16
-#       pixel[x+1][y+1] := pixel[x+1][y+1] + quant_error * 1/16
-#
-# find_closest_palette_color(oldpixel) = floor(oldpixel / 256)
-
 from argparse import ArgumentParser
 from math import floor
 from PIL import Image
@@ -52,6 +35,25 @@ class Dither():
         return 255 * floor(value/128)
 
     def floyd_steinberg_dither(self, image_file):
+        """
+        https://en.wikipedia.org/wiki/Floyd–Steinberg_dithering
+
+        Pseudocode:
+
+        for each y from top to bottom
+           for each x from left to right
+              oldpixel  := pixel[x][y]
+              newpixel  := find_closest_palette_color(oldpixel)
+              pixel[x][y]  := newpixel
+              quant_error  := oldpixel - newpixel
+              pixel[x+1][y  ] := pixel[x+1][y  ] + quant_error * 7/16
+              pixel[x-1][y+1] := pixel[x-1][y+1] + quant_error * 3/16
+              pixel[x  ][y+1] := pixel[x  ][y+1] + quant_error * 5/16
+              pixel[x+1][y+1] := pixel[x+1][y+1] + quant_error * 1/16
+
+        find_closest_palette_color(oldpixel) = floor(oldpixel / 256)
+        """
+
         new_img = Image.open(image_file)
 
         new_img = new_img.convert('RGB')
